b1f19a382d7b0cf3bab00c33c86ba605f157ea73
cayk11 <62639029+cayk11@users.noreply.github.com>
2020-03-25 15:05:20 +0800

    A Python module with a function

---
+++ mymodule.py
@@ -0,0 +1,16 @@
+from urlib.request import urlopen
+import json
+
+api_key = "xxxxxxx"
+
+def get_weather(city):
+    sock = urlopen("http://api.openweathermap.org/data/2.5/weather?q=" +
+                           city + "&appid=" + api_key)
+    result = sock.read()
+    sock.close()
+    weather = json.loads(results)
+    return weather["main]["temp"] -273.15
+    
+if __name__ == "__main__":
+    degrees = get_weather("OLSO")
+    print("Weather in Olso is %.2f degrees Celsius" % degrees)
